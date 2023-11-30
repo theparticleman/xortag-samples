f9f01301948ae31db3d425582be3f9511d7714d1
Jon Turner <jturner@samcart.com>
2023-11-30 09:48:49 -0700

    Add C# example

---
+++ csharp/Program.cs
@@ -0,0 +1,81 @@
+﻿using RestSharp;
+
+const string Url = "https://xortag.azurewebsites.net/";
+ResponseViewModel world;
+Random rand = new();
+var restClient = new RestClient(Url);
+
+Register();
+Move();
+
+void Move()
+{
+  while (true)
+  {
+    switch (rand.Next(4))
+    {
+      case 0:
+        MoveUp();
+        break;
+      case 1:
+        MoveRight();
+        break;
+      case 2:
+        MoveDown();
+        break;
+      case 3:
+        MoveLeft();
+        break;
+    }
+  }
+}
+
+void MoveUp() => world = SendMoveCommand("moveUp");
+void MoveDown() => world = SendMoveCommand("moveDown");
+void MoveRight() => world = SendMoveCommand("moveRight");
+void MoveLeft() => world = SendMoveCommand("moveleft");
+
+ResponseViewModel SendMoveCommand(string moveCommand)
+{
+  Thread.Sleep(1000); //Requests more frequent than once per second will fail.
+  CheckForRegistration();
+  return restClient.GetJson<ResponseViewModel>($"{moveCommand}/{world.Id}");
+}
+
+void Look()
+{
+  Thread.Sleep(1000); //Requests more frequent than once per second will fail.
+  CheckForRegistration();
+  world = restClient.GetJson<ResponseViewModel>($"look/{world.Id}");
+}
+
+void CheckForRegistration()
+{
+  if (world == null) throw new InvalidOperationException("You have to register as a player before you can move");
+}
+
+void Register()
+{
+  world = restClient.GetJson<ResponseViewModel>("register");
+  Console.WriteLine("You have successfully registered!");
+  Console.WriteLine("Your player name is {0} and your id is {1}", world.Name, world.Id);
+}
+
+record ResponseViewModel
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public bool IsIt { get; set; }
+  public int X { get; set; }
+  public int Y { get; set; }
+  public int MapWidth { get; set; }
+  public int MapHeight { get; set; }
+  public List<PlayerViewModel> Players { get; set; }
+}
+
+record PlayerViewModel
+{
+  public int X { get; set; }
+  public int Y { get; set; }
+  public bool IsIt { get; set; }
+}
---
+++ csharp/readme.md
@@ -0,0 +1,5 @@
+This code was last tested using .NET 6.0.402.
+
+To run the code, use the command `dotnet run` in the current directory. .NET 6.0 SDK must be installed and in the path for this to work.
+
+If you don't have .NET installed but have Docker installed, you can run the code with the following command: `docker run --rm -it -w /app -v ${PWD}:/app mcr.microsoft.com/dotnet/sdk:6.0 dotnet run`.
